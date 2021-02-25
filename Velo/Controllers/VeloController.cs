ba642596731b5f14eeecd37bf54da366f90b0756
hypernova0910 <ngminhhieu2000910@gmail.com>
2021-02-25 08:55:10 +0700

    Add project files.

---
+++ Velo/Controllers/VeloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Velo.Controllers
+{
+    public class VeloController : Controller
+    {
+        // GET: Velo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
