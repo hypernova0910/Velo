028302afeaeddd259506295345469f47e4ec60e6
Nguyễn Lộc <loc3120@gmail.com>
2021-04-25 01:33:09 +0700

    Thêm edit + lưu đường dẫn ảnh

--- Velo/assets/js/eventHomePage.js
+++ Velo/assets/js/eventHomePage.js
@@ -38,6 +38,17 @@ $('#notification').on('click', function (e) {
     })
 })
 
+$('#edit').on('click', function (e) {
+    $('.personal-info-area').empty()
+    $.ajax({
+        url: "/Velo/Edit/" + $('#id-user').val(),
+    }).done(function (rs) {
+        $('.personal-info-area').append(rs)
+    }).fail(function (err) {
+        console.log(err)
+    })
+})
+
 $(function () {
     $.ajax({
         url: "/Velo/Photos/" + $('#id-user').val(),
@@ -47,6 +58,58 @@ $(function () {
         console.log(err)
     })
 })
+
+//function myFunction(id) {
+//    document.getElementById(id).classList.toggle("show");
+//}
+//var buttons = document.getElementsByClassName('info-frame');
+//var contents = document.getElementsByClassName('account-info');
+//for (var i = 0; i < buttons.length; i++) {
+//    buttons[i].addEventListener("click", function () {
+//        //lấy value của button
+//        var id = this.title;
+//        //ẩn tất cả các menu con đang được hiển thị
+//        for (var i = 0; i < contents.length; i++) {
+//            contents[i].classList.remove("show");
+//        }
+//        //hiển thị menu vừa được click
+//        myFunction(id);
+//    });
+//}
+//window.addEventListener("click", function () {
+//    if (!event.target.matches('.info-frame') && !event.target.matches('.account-info-detail') ) {
+//        for (var i = 0; i < contents.length; i++) {
+//            contents[i].classList.remove("show");
+//        }
+//    }
+//});
+
+
+
+$(document).ready(function () {
+
+    var readURL = function (input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $(".form-avatar").on('change', function () {
+        readURL(this);
+    });
+
+    $(".upload-button").on('click', function () {
+        $(".form-avatar").click();
+    });
+});
+
+
 
 //$('.chat-input input').keyup(function (e) {
 //    if ($(this).val() == '')
